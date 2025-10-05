bdf5c325f808719e15589c3cd3601d4605a95ebc
PelucaP <tomas.carcamo.g@usach.cl>
2025-10-05 00:05:56 -0300

    primer commit, base de datos configurada con docker compose

---
+++ src/main/java/com/innova/flota/InnovaApplication.java
@@ -0,0 +1,12 @@
+package com.innova.flota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/innova/flota/InnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innova.flota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
